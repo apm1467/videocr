51ab92cca4e15de4695cb07c756488c6cd22f663
Yi Ge <me@yige.ch>
2019-12-15 21:49:57 +0800

    reformat setup.py

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@ with open('README.md', 'r', encoding='utf-8') as f:
     readme = f.read()
 
 setup(
-    name = 'videocr',
-    packages = ['videocr'],
-    version = '0.1.5',
-    license = 'MIT',
-    description = 'Extract hardcoded subtitles from videos using machine learning',
-    long_description_content_type = 'text/markdown',
-    long_description = readme,
-    author = 'Yi Ge',
-    author_email = 'me@yige.ch',
-    url = 'https://github.com/apm1467/videocr',
-    download_url = 'https://github.com/apm1467/videocr/archive/v0.1.5.tar.gz',
-    install_requires = [
+    name='videocr',
+    packages=['videocr'],
+    version='0.1.5',
+    license='MIT',
+    description='Extract hardcoded subtitles from videos using machine learning',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    author='Yi Ge',
+    author_email='me@yige.ch',
+    url='https://github.com/apm1467/videocr',
+    download_url='https://github.com/apm1467/videocr/archive/v0.1.5.tar.gz',
+    install_requires=[
         'fuzzywuzzy>=0.17',
         'python-Levenshtein>=0.12',
         'opencv-python>=4.1,<5.0',
         'pytesseract>=0.2.6'
     ],
-    classifiers = [
-      'Development Status :: 3 - Alpha',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: MIT License',
-      'Programming Language :: Python :: 3',
-      'Programming Language :: Python :: 3.7',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
 )
