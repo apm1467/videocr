678919df33e537fd9b017c464343c37e4891ac8b
Raphaël Droz <raphael.droz@gmail.com>
2023-04-17 17:53:45 -0300

    fix TESSDATA_*URL
    Following Tesseract OCR branch naming changes

--- videocr/constants.py
+++ videocr/constants.py
@@ -2,6 +2,6 @@ import pathlib
 
 TESSDATA_DIR = pathlib.Path.home() / 'tessdata'
 
-TESSDATA_URL = 'https://github.com/tesseract-ocr/tessdata_fast/raw/master/{}.traineddata'
+TESSDATA_URL = 'https://github.com/tesseract-ocr/tessdata_fast/raw/main/{}.traineddata'
 
-TESSDATA_SCRIPT_URL = 'https://github.com/tesseract-ocr/tessdata_best/raw/master/script/{}.traineddata'
+TESSDATA_SCRIPT_URL = 'https://github.com/tesseract-ocr/tessdata_best/raw/main/script/{}.traineddata'
